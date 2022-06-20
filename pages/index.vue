aafc11ec09493b1b25edfc39fca6600cdc5b1c11
maulik.pilojpara <maulik.pilojpara@augustinfotechteam.com>
2022-06-21 00:01:16 +0530

    Seasonal products on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
     <Category />
     <ProductSlider v-if="popularProducts && popularProducts.length > 0" title="Popular Products" :products="popularProducts" />
     <ProductSlider v-if="recentProducts && recentProducts.length > 0" title="Recent Products" :products="recentProducts" />
+    <ProductSlider v-if="seasonItemArray && seasonItemArray.length > 0" title="Seasonal Products" :products="seasonItemArray" />
     <Testimonials />
     <Certifications />
   </div>
@@ -24,7 +25,10 @@ export default {
     })
   },
   // async fetch (context) {
-  //   await context.store.dispatch('getWebsiteMainContent');
+  //   const appURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:10000' : process.env.APP_URL_PROD;
+  //   const healthySnacks = await context.app.$axios.get(`${appURL}/api/getproducts/dairy-eggs`);
+  //   console.log('healthySnacks::: ', healthySnacks.data);
+  //   // await context.store.dispatch('getWebsiteMainContent');
   // },
   components: {
     MainBanner,
@@ -34,16 +38,19 @@ export default {
     Testimonials: () => import('~/components/home/Testimonials.vue'),
     Certifications: () => import('~/components/home/Certifications.vue')
   },
-  // async asyncData({$axios}) {
-  //   const appURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:10000' : process.env.APP_URL_PROD;
-  //   const healthySnacks = await $axios.get(`${appURL}/api/getproducts/healthy-snacks`);
-  //   console.log('healthySnacks::: ', healthySnacks);
-  //   return {
-  //     healthySnacks
-  //   }
-  // },
+  async asyncData({$axios}) {
+    console.log('asyncData');
+    const appURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:10000' : process.env.APP_URL_PROD;
+    const seasonItem = await $axios.get(`${appURL}/api/get_season_item`);
+    const seasonItemArray = seasonItem?.data?.message
+    console.log('seasonItem::: ', seasonItem);
+    return {
+      seasonItemArray
+    }
+  },
   // async mounted () {
   //   const appURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:10000' : process.env.APP_URL_PROD;
+  //   console.log('mountedin', appURL);
   //   const healthySnacks = await this.$axios.get(`${appURL}/api/getproducts/healthy-snacks`);
   //   console.log('healthySnacks::: ', healthySnacks);
   // }
