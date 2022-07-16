f54aa1075420d8f75fa20efc9ca0ef29d43d8b00
maulik.pilojpara <maulik.pilojpara@augustinfotechteam.com>
2022-07-16 14:48:13 +0530

    Add why sahaja and combo bundle section on homepage

--- components/home/MainBanner.vue
+++ components/home/MainBanner.vue
@@ -1,98 +1,16 @@
 <template>
-  <section class="mini-section pt-md-0" v-if="homeBanners && homeBanners.length > 0">
+  <section class="mini-section pt-md-0" v-if="items && items.length > 0">
     <div class="container">
       <hooper class="main-slider" :settings="mainBannerSetting">
-        <template v-for="(banner, indx) in homeBanners">
-          <slide class="item" v-if="banner && banner.banner_image" :key="indx">
+        <template v-for="(banner, indx) in items">
+          <slide class="item" v-if="banner && (banner.banner_image || banner.image)" :key="indx">
             <div class="banner-box">
               <div class="banner-img">
-                <img :src="banner.banner_image" class="d-block w-100" />
+                <img :src="banner.banner_image ? banner.banner_image : banner.image" class="d-block w-100" />
               </div>
-              <!-- <div class="banner-text">
-                <div class="banner-logo">
-                  <img src="/logo-light.svg" alt="" />
-                </div>
-                <h2>We Do Healthy</h2>
-                <p>
-                  Lorem ipsum is a placeholder text commonly used to demonstrate
-                  the visual form of a document or a typeface without relying on
-                  meaningful content.
-                </p>
-                <div class="btn-set">
-                  <a href="#" class="btn btn-light">Shop Now</a>
-                </div>
-                <h5>100% Organic Guarantee</h5>
-              </div> -->
             </div>
           </slide>
         </template>
-        <!-- <hooper-pagination slot="hooper-addons"></hooper-pagination> -->
-        <!-- <div class="item">
-          <div class="banner-box">
-            <div class="banner-img">
-              <img src="/banner.jpg" class="d-block w-100" />
-            </div>
-            <div class="banner-text">
-              <div class="banner-logo">
-                <img src="/logo-light.svg" alt="" />
-              </div>
-              <h2>We Do Healthy</h2>
-              <p>
-                Lorem ipsum is a placeholder text commonly used to demonstrate
-                the visual form of a document or a typeface without relying on
-                meaningful content.
-              </p>
-              <div class="btn-set">
-                <a href="#" class="btn btn-light">Shop Now</a>
-              </div>
-              <h5>100% Organic Guarantee</h5>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <div class="banner-box">
-            <div class="banner-img">
-              <img src="/banner.jpg" class="d-block w-100" />
-            </div>
-            <div class="banner-text">
-              <div class="banner-logo">
-                <img src="/logo-light.svg" alt="" />
-              </div>
-              <h2>We Do Healthy</h2>
-              <p>
-                Lorem ipsum is a placeholder text commonly used to demonstrate
-                the visual form of a document or a typeface without relying on
-                meaningful content.
-              </p>
-              <div class="btn-set">
-                <a href="#" class="btn btn-light">Shop Now</a>
-              </div>
-              <h5>100% Organic Guarantee</h5>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <div class="banner-box">
-            <div class="banner-img">
-              <img src="/banner.jpg" class="d-block w-100" />
-            </div>
-            <div class="banner-text">
-              <div class="banner-logo">
-                <img src="/logo-light.svg" alt="" />
-              </div>
-              <h2>We Do Healthy</h2>
-              <p>
-                Lorem ipsum is a placeholder text commonly used to demonstrate
-                the visual form of a document or a typeface without relying on
-                meaningful content.
-              </p>
-              <div class="btn-set">
-                <a href="#" class="btn btn-light">Shop Now</a>
-              </div>
-              <h5>100% Organic Guarantee</h5>
-            </div>
-          </div>
-        </div> -->
         <hooper-navigation slot="hooper-addons"></hooper-navigation>
       </hooper>
     </div>
@@ -106,6 +24,12 @@ import { mapGetters  } from 'vuex'
 
 export default {
   name: 'MainBanner',
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
   data() {
     return {
       mainBannerSetting: {
@@ -125,11 +49,11 @@ export default {
     HooperPagination,
     HooperNavigation
   },
-  computed: {
-    ...mapGetters({
-      homeBanners: 'global/getHomeBanners'
-    })
-  }
+  // computed: {
+  //   ...mapGetters({
+  //     homeBanners: 'global/getHomeBanners'
+  //   })
+  // }
 };
 </script>
 
